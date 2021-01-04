8f7d9d590479e997bb7ab792ee61e5bf249e965b
mac <adewumi.alabi@fundall.io>
2021-01-04 09:28:58 +0100

    Updates

--- pages/index.vue
+++ pages/index.vue
@@ -52,6 +52,9 @@
             </div>
           </v-card>
         </v-flex>
+        <v-flex v-if="filteredProducts.length == 0" md12 text-center>
+          <img src="../static/assets/no-products-found.png" alt="" />
+        </v-flex>
       </v-layout>
     </v-container>
     <v-dialog width="600" v-model="productDialog">
@@ -76,6 +79,7 @@
   </div>
 </template>
 <script>
+import { products } from '../util/products'
 export default {
   data() {
     return {
@@ -83,164 +87,40 @@ export default {
       searchString: '',
       category: '',
       singleProduct: [],
+      products: products,
       sliders: [
         require('../static/assets/slide1.png'),
         require('../static/assets/slide2.png'),
         require('../static/assets/slide3.png'),
       ],
-      products: [
-        {
-          id: 1,
-          name: 'Mama Gold Rice',
-          image: require('../static/assets/rice.jpg'),
-          category: 'Product Merchant',
-          price: 23000,
-        },
-        {
-          id: 2,
-          name: 'Wrist Watch',
-          image: require('../static/assets/wristwatch.jpg'),
-          category: 'Service Merchant',
-          price: 10000,
-        },
-        {
-          id: 3,
-          name: 'Wachine Machine',
-          image: require('../static/assets/washing machine.jpg'),
-          category: 'Product Merchant',
-          price: 60000,
-        },
-        {
-          id: 4,
-          name: 'Gas Cooker',
-          image: require('../static/assets/gas cooker.jpg'),
-          category: 'Service Merchant',
-          price: 30000,
-        },
-        {
-          id: 5,
-          name: 'Vinegar Sellamin',
-          image: require('../static/assets/vinegar.jpg'),
-          category: 'Product Merchant',
-        },
-        {
-          id: 6,
-          name: 'Thermacool Refrigerator',
-          image: require('../static/assets/refrigerator.jpg'),
-          category: 'Product Merchant',
-          price: 150000,
-        },
-        {
-          id: 7,
-          name: 'Maggi Chicken',
-          image: require('../static/assets/maggi.jpg'),
-          category: 'Service Merchant',
-          price: 2000,
-        },
-        {
-          id: 8,
-          name: 'Milo Beverage',
-          image: require('../static/assets/milo.jpg'),
-          price: 1500,
-        },
-        {
-          id: 9,
-          name: 'Phillips Pressing Iron',
-          image: require('../static/assets/iron.jpg'),
-          category: 'Product Merchant',
-        },
-        {
-          id: 10,
-          name: 'Electric Kettle',
-          image: require('../static/assets/kettle.jpg'),
-          category: 'Product Merchant',
-          price: 7000,
-        },
-        {
-          id: 11,
-          name: 'HP Printer',
-          image: require('../static/assets/printer.png'),
-          price: 40000,
-        },
-        {
-          id: 12,
-          name: 'Fitted Shirt',
-          image: require('../static/assets/shirt.png'),
-          category: 'Product Merchant',
-          price: 6000,
-        },
-        {
-          id: 13,
-          name: 'Toasting Machine',
-          image: require('../static/assets/toaster.jpg'),
-          category: 'Service Merchant',
-          price: 23000,
-        },
-        {
-          id: 14,
-          name: 'Brazillian Wig',
-          image: require('../static/assets/wig.jpg'),
-          category: 'Service Merchant',
-          price: 90000,
-        },
-        {
-          id: 15,
-          name: 'PS4 Game',
-          image: require('../static/assets/ps4.jpg'),
-          category: 'Product Merchant',
-          price: 560000,
-        },
-        {
-          id: 16,
-          name: 'Barbie Doll',
-          image: require('../static/assets/doll.jpg'),
-          price: 4000,
-        },
-        {
-          id: 17,
-          name: 'Fotomi',
-          image: require('../static/assets/fotomi.jpg'),
-          category: 'Product Merchant',
-          price: 40000,
-        },
-        {
-          id: 18,
-          name: 'Golden Morn',
-          image: require('../static/assets/golden morn.jpg'),
-          category: 'Service Merchant',
-          price: 2000,
-        },
-        {
-          id: 19,
-          name: 'iPhone',
-          image: require('../static/assets/iphone.png'),
-          category: 'Service Merchant',
-          price: 360000,
-        },
-        {
-          id: 20,
-          name: 'Bluetooth Speaker',
-          image: require('../static/assets/Bluetooth Speaker.jpg'),
-          category: 'Service Merchant',
-          price: 14000,
-        },
-      ],
     }
   },
   watch: {
     category: function () {
+      this.products = products
+      this.selectProducts()
+    },
+    searchString: function () {
+      this.products = products
+    }
+  },
+  methods: {
+    selectProducts() {
       var products = this.products
-      products = products.filter(function (item) {
-        if (item.category.toLowerCase().indexOf(this.category) !== -1) {
-          return item
+      var selectedProducts = []
+      for (var i = 0; i < products.length; i++) {
+        if (products[i].category == this.category) {
+          selectedProducts.push(products[i])
         }
-      })
+      }
+      this.products = selectedProducts
     },
   },
   computed: {
     filteredProducts: function () {
       var products = this.products
       var searchString = this.searchString
+      var category = this.category
 
       if (!searchString) {
         return products
@@ -253,7 +133,6 @@ export default {
           return item
         }
       })
-
       return products
     },
   },
